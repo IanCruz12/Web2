8b55e4544ddb8e62a3a27d81df9f3618b9e6964a
IanCruz12 <125672282+IanCruz12@users.noreply.github.com>
2024-03-08 20:48:32 -0600

    Add files via upload

---
+++ Resultado.php
@@ -0,0 +1,17 @@
+<?php
+        echo "<b>Datos generales</b><br><br>";
+        echo "Su Nombre es: <b> {$_POST['Nombre']} {$_POST['ApellidoP']} {$_POST['ApellidoM']} </b><br>";
+        echo "Su Nombre de usuario es: <b>{$_POST['usuario']}</b><br>";
+        echo "Su Contraseña es: <b>{$_POST['contra']}</b><br>";
+        echo "Su Fecha de nacimiento es: <b>{$_POST['Fech']}</b><br><br>";
+
+        echo "<b>Datos de facturacion</b><br><br>";
+        echo "Su Pais es: <b>{$_POST['pais']}</b><br>";
+        echo "Su Calle es: <b>{$_POST['calle']}</b><br>";
+        echo "Su Numero de Casa es: <b>{$_POST['numcasa']}</b><br>";
+        echo "Su Colonia es: <b>{$_POST['colonia']}</b><br>";
+        echo "Su E-mail es: <b>{$_POST['email']}</b><br>";
+        echo "Su RFC es: <b>{$_POST['rfc']}</b><br>";
+        echo "Su CP es: <b>{$_POST['cp']}</b><br>";
+        
+?>
